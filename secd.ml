7b449302d729b770cc8b4b11fe5e254e306a71f1
zehnpaard <zehnpaard@googlemail.com>
2018-12-08 11:59:55 +0900

    Add getn, locate and LD

--- secd.ml
+++ secd.ml
@@ -18,6 +18,10 @@ let makeCons i j =
   let n = !f in
   (cells.(n) <- Cons (i, j); f := n + 1; n)
 
+let getn i = match cells.(i) with
+  | Int n -> n
+  | _ -> failwith "Getting number from cons cell"
+
 let car i = match cells.(i) with
   | Cons (i, _) -> i
   | _ -> failwith "Taking car of non-cons cell"
@@ -33,11 +37,26 @@ let popR r = match cells.(!r) with
 let pushR r i =
   r := makeCons i !r
 
+let rec locate i j =
+  let rec locatej j env =
+    if j = 0 then car env
+    else locatej (j - 1) (cdr env)
+  in
+  let rec locatei i j env =
+    if i = 0 then locatej j (car env)
+    else locatei (i - 1) j (cdr env)
+  in
+  locatei i j !e
+
 let runOneStep () = match cells.(popR c) with
   | Int 0 (* STOP *) -> c := 0
   | Int 1 (* NIL *) -> pushR s 0
   | Int 2 (* LDC *) -> pushR s (popR c)
-  | Int 3 (* LD *) -> e
+  | Int 3 (* LD *) ->
+      let ij = popR c in
+      let i = getn (car ij) in
+      let j = getn (cdr ij) in
+      pushR s (locate i j)
   | Int _ -> failwith "Unknown command"
   | _ -> failwith "Cons cell found in place of command"
 
