daff98b99cd900418f92bbea6d7afbafbe9684fc
zehnpaard <zehnpaard@googlemail.com>
2018-12-11 20:20:09 +0900

    Stop opening Exp inside Compile

--- compile.ml
+++ compile.ml
@@ -1,5 +1,3 @@
-open Exp
-
 let index vname names =
   let rec indx names i = match names with
     | [] -> failwith "Variable " ^ vname ^ " not found in environment"
@@ -14,23 +12,23 @@ let index vname names =
   indx names 0
 
 let rec compile exp names acc = match exp with
-  | Nil -> C.nil :: acc
-  | Int n -> C.ldc :: n :: acc
-  | Var s -> C.ld :: index s names :: acc
-  | If (cond, tcase, fcase) -> compile_if cond tcase fcase names acc
-  | Lambda (params, body) -> compile_lambda body (params :: names) acc
-  | Let (vars, exps, body) ->
+  | Exp.Nil -> C.nil :: acc
+  | Exp.Int n -> C.ldc :: n :: acc
+  | Exp.Var s -> C.ld :: index s names :: acc
+  | Exp.If (cond, tcase, fcase) -> compile_if cond tcase fcase names acc
+  | Exp.Lambda (params, body) -> compile_lambda body (params :: names) acc
+  | Exp.Let (vars, exps, body) ->
       let cbody = compile_lambda body (vars :: names) (C.ap :: acc) in
       C.nil :: compile_app exps names cbody
-  | LetRec (vars, exps, body) ->
+  | Exp.LetRec (vars, exps, body) ->
       let names' = vars :: names in
       let cbody = compile_lambda body names' (C.rap :: acc) in
       C.dum :: C.nil :: compile_app exps names' cbody
-  | Call (Var fname, args) when List.mem fname builtins ->
+  | Exp.Call (Exp.Var fname, args) when List.mem fname builtins ->
       compile_builtin fname args names acc
-  | Call (Var fname, args) ->
+  | Exp.Call (Exp.Var fname, args) ->
       let cfunc = C.ld :: index fname names :: C.ap :: acc in
       C.nil :: compile_app args names cfunc
-  | Call (exp1, args) ->
+  | Exp.Call (exp1, args) ->
       let cfunc = compile exp1 names (C.ap :: acc) in
       C.nil :: compile_app args names cfunc
