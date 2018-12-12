8cceddc5cb8db83798c7370702ce04c5169f0b8b
zehnpaard <zehnpaard@googlemail.com>
2018-12-12 22:11:05 +0900

    Change compilation target to ADT representing SECD code

--- compile.ml
+++ compile.ml
@@ -1,3 +1,15 @@
+type t =
+  | Nil
+  | Ldc of int
+  | Ld of int * int
+  | Ap
+  | Rap
+  | Dum
+  | Sel
+  | Join
+  | Ldf of t list
+  | Sel of t list * t list
+
 let index vname names =
   let rec indx names i = match names with
     | [] -> failwith "Variable " ^ vname ^ " not found in environment"
@@ -12,23 +24,30 @@ let index vname names =
   indx names 0
 
 let rec compile exp names acc = match exp with
-  | Exp.Nil -> C.nil :: acc
-  | Exp.Int n -> C.ldc :: n :: acc
-  | Exp.Var s -> C.ld :: index s names :: acc
+  | Exp.Nil -> Nil :: acc
+  | Exp.Int n -> Ldc n :: acc
+  | Exp.Var s -> Ld (index s names) :: acc
   | Exp.If (cond, tcase, fcase) -> compile_if cond tcase fcase names acc
   | Exp.Lambda (params, body) -> compile_lambda body (params :: names) acc
   | Exp.Let (vars, exps, body) ->
-      let cbody = compile_lambda body (vars :: names) (C.ap :: acc) in
-      C.nil :: compile_app exps names cbody
+      let cbody = compile_lambda body (vars :: names) (Ap :: acc) in
+      Nil :: compile_app exps names cbody
   | Exp.LetRec (vars, exps, body) ->
       let names' = vars :: names in
-      let cbody = compile_lambda body names' (C.rap :: acc) in
-      C.dum :: C.nil :: compile_app exps names' cbody
+      let cbody = compile_lambda body names' (Rap :: acc) in
+      Dum :: Nil :: compile_app exps names' cbody
   | Exp.Call (Exp.Var fname, args) when List.mem fname builtins ->
       compile_builtin fname args names acc
   | Exp.Call (Exp.Var fname, args) ->
-      let cfunc = C.ld :: index fname names :: C.ap :: acc in
-      C.nil :: compile_app args names cfunc
+      let cfunc = Ld (index fname names) :: Ap :: acc in
+      Nil :: compile_app args names cfunc
   | Exp.Call (exp1, args) ->
-      let cfunc = compile exp1 names (C.ap :: acc) in
-      C.nil :: compile_app args names cfunc
+      let cfunc = compile exp1 names (Ap :: acc) in
+      Nil :: compile_app args names cfunc
+  and compile_if cond tcase fcase names acc =
+    let ctcase = compile tcase names [Join] in
+    let cfcase = compile fcase names [Join] in
+    let acc' = Sel (ctcase, cfcase) :: acc in
+    compile cond names acc'
+  and compile_lambda body names acc =
+    Ldf (compile body names [Rtn]) :: acc
